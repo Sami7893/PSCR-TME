647cfa7ba8fcdaeb6881263b08774693ba248d23
Sami7893 <benhammou7878@gmail.com>
2025-10-02 23:24:13 +0200

    TME 1 Terminé

--- TME1/TestString/src/String.cpp
+++ TME1/TestString/src/String.cpp
@@ -1,22 +1,75 @@
 #include "String.h"
-
+#include <iostream>
 namespace pr
 {
 
 // TODO: Implement constructor e.g. using initialization list
 String::String (const char *s)
 {
-  std::cout << "String constructor called for: " << s << std::endl;
+  data = newcopy(s);
+  std::cout << "String constructor called for: " << data << std::endl;
+}
+
+
+String::String(const String& other):String(other.data){}
+
+
+String::String(String&& other)noexcept{
+  data = other.data;
+  other.data = nullptr;
 }
 
 String::~String ()
 {
   std::cout << "String destructor called for: " << (data ? data : "(null)")
       << std::endl;
-  // TODO
+  delete[] data;
 }
 
+
 // TODO : add other operators and functions
+std::ostream& operator<<(std::ostream& os, const String& str){
+  return os<<str.data;
+}
+
+
+bool String::operator<(const String& other) const{
+  int result = compare(data, other.data);
+  return result < 0;
+}
+
+
+String &String::operator=(const String &other){
+  if(this != &other){
+    delete []data;
+    data = newcopy(other.data);
+  }
+  return *this;
+}
+
+
+String& String::operator=(String&& other) noexcept{
+  if(this != &other){
+    delete [] data;
+    data = other.data;
+    other.data = nullptr;
+  }
+  return *this;
+}
+
+
+bool operator==(const String& a, const String& b){
+  return compare(a.data,b.data) == 0;
+}
+
+
+String operator+(const String& a, const String& b){
+  char* tmp = newcat(a.data, b.data);  
+  String result(tmp);                   
+  delete[] tmp;                         
+  return result; 
+}
+
 
 }// namespace pr
 
--- TME1/TestString/src/strutil.cpp
+++ TME1/TestString/src/strutil.cpp
@@ -1,21 +1,43 @@
 // strutil.cpp
 #include "strutil.h"
-
+#include <iostream>
 namespace pr {
 
 size_t length(const char* s) {
-
-    return 0;
+    size_t y = 0;
+    for(;*s;++s){
+        ++y;
+    }
+    return y;
 }
 
 char* newcopy(const char* s) {
-
-    return nullptr;
+    size_t len = length(s);
+    char* nc = new char[len+1];
+    for(size_t i=0;i<=len;++i){
+        nc[i] = s[i];
+    }
+    return nc;
 }
 
 int compare(const char* a, const char* b) {
-
-    return 0;
+    for(;*a == *b && *a;++a,++b){}
+    return *a - *b;
 }
 
+char* newcat(const char* a, const char* b) {
+    size_t len_a = length(a);
+    size_t len_b = length(b);
+    char* concat = new char[len_a + len_b + 1];
+    
+    for (size_t i = 0; i < len_a; ++i) {
+        concat[i] = a[i];
+    }
+    for (size_t i = 0; i < len_b; ++i) {
+        concat[len_a + i] = b[i];
+    }
+
+    concat[len_a + len_b] = '\0';
+    return concat;
+}
 }
